70562139b990325f20ffbe52598472dab6e41bb5
SameerR007 <100122935+SameerR007@users.noreply.github.com>
2023-03-31 12:40:42 +0530

    Add files via upload

---
+++ article_summarizer.py
@@ -0,0 +1,17 @@
+from newspaper import Article
+import streamlit as st
+st.title("Article Summarizer")
+url =st.text_input('Paste the article link below',"")
+language={"English":"en","Hindi":"hi"}
+selected_lang=st.selectbox("Language of the article", language)
+if st.button("Summarize"):
+    my_article = Article(url, selected_lang)
+    my_article.download()
+    my_article.parse()
+    st.header(my_article.title)
+    # NLP on the article
+    my_article.nlp()
+    # Extract summary
+    st.markdown(my_article.summary)
+    # Extract keywords
+    st.markdown('Keywords: '+','.join([i for i in (my_article.keywords)]))
